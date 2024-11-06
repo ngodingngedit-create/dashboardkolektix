06fe3b792ef6a807f4efb7d4a316769fe7809981
Rizqy Candra Saputra <19533728-rizqycndra@users.noreply.gitlab.com>
2024-11-07 01:08:54 +0700

    [chore] add merch invoice page file

---
+++ src/pages/merch-invoice/[invoice].tsx
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+
+export default function Invoice() {
+
+    return (
+        <div className={`bg-primary-light mt-[-20px] pt-[20px] pb-[30px] mb-[-20px]`}>
+
+        </div>
+    );
+}
