145a45f70a3d38047d54d4ff5c5e113876fab06f
Rizqy Candra Saputra <19533728-rizqycndra@users.noreply.gitlab.com>
2024-12-02 21:07:12 +0700

    [fix] event-order: order free witout auth

--- src/components/Payment/FirstStepUnlogged.tsx
+++ src/components/Payment/FirstStepUnlogged.tsx
@@ -228,7 +228,6 @@ const FirstStepUnlogged = ({ detail, ticket, totalCount, totalSubtotalPrice, for
     const router = useRouter();
 
     const submitForm = () => {
-        setLoading(true);
         const now = new Date();
         now.setTime(now.getTime() + 24 * 60 * 60 * 1000);
         const isoString = now.toISOString();
@@ -244,10 +243,11 @@ const FirstStepUnlogged = ({ detail, ticket, totalCount, totalSubtotalPrice, for
         };
 
         const ticketPriceTotal = ticket.reduce((e, n) => e + n.price * n.qty_ticket, 0);
-        if (ticketPriceTotal == 0) {
-            return;
-        }
+        // if (ticketPriceTotal == 0) {
+        //     return;
+        // }
 
+        setLoading(true);
         Post('transaction-without-auth', payload)
             .then((res: any) => {
                 setTransactionData(res.data);
