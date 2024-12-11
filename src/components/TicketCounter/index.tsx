35afea98d23950493d5de0ff05eb45c1dcc65c0a
Rizqy Candra Saputra <19533728-rizqycndra@users.noreply.gitlab.com>
2024-12-12 02:17:00 +0700

    [chore] TicketCounter: implement new condition

--- src/components/TicketCounter/index.tsx
+++ src/components/TicketCounter/index.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
-import { ActionIcon, Badge, Box, Flex, Text } from '@mantine/core';
+import { ActionIcon, Badge, Box, Flex, Stack, Text } from '@mantine/core';
 import moment from 'moment';
 import { TicketProps } from '@/utils/globalInterface';
 import { Icon } from '@iconify/react/dist/iconify.js';
@@ -141,40 +141,23 @@ const TicketCounter = ({
 
   return (
     <div
-      className={`border ${
-        isSoldOut
-          ? 'bg-[#ffebec] border-[#ffebec]'
-          : isFinish
-          ? 'bg-primary-light border-primary-light-200'
-          : isReady
-          ? 'bg-gray-200 border-gray-200'
-          : 'border-primary-light-200'
-      } rounded-md flex flex-col shadow-sm mb-5 divide-y-2 divide-primary-light-200 divide-dashed bg-primary-light`}
+      className={`border border-primary-disabled/50 rounded-lg flex flex-col shadow-sm mb-5 divide-y-2 divide-primary-light-200 divide-dashed bg-primary-light`}
     >
-      <div className='p-3'>
-        <p className=''>{title}</p>
+      <div className='p-3 flex justify-between'>
+        <Stack gap={5}>
+          <p className=''>{title}</p>
+          {ticketData.description && (
+              <Text size="sm" c="gray">
+                  {ticketData.description?.split('\n').map((e, i) => (
+                      <Text key={i}>{e}</Text>
+                  ))}
+              </Text>
+          )}
+        </Stack>
+        <p className='font-semibold'>Rp {price.toLocaleString('id-ID')}</p>
       </div>
       <div className='flex items-center justify-between p-3'>
-        <p className='font-semibold'>Rp {price.toLocaleString('id-ID')}</p>
-        <div className='flex items-center gap-3'>
-          {isSoldOut ? (
-            <button className='bg-[#ff9292] text-[#870809] px-3 py-1 text-sm font-semibold rounded-2xl'>
-              Sold Out
-            </button>
-          ) : isFinish ? (
-            <button className='bg-primary-light-200 text-primary-disabled px-3 py-1 text-sm font-semibold rounded-2xl'>
-              Event Selesai
-            </button>
-          ) : isReady ? (
-            <button className='bg-gray-300 text-gray-500 px-3 py-1 text-sm font-semibold rounded-2xl' disabled>
-              Belum di mulai
-            </button>
-          ) : (
-            <>
-              <StatusComponent />
-            </>
-          )}
-        </div>
+        <StatusComponent />
       </div>
     </div>
   );
