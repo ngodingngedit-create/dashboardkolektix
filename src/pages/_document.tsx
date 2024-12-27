39aee4b1c7521b8f114b12dafaf4db15e245d241
Rizqy Candra Saputra <19533728-rizqycndra@users.noreply.gitlab.com>
2024-12-28 00:52:25 +0700

    [chore] snippet google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,23 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang='en'>
-      <Head />
+    <Html>
+      <Head>
+        {/* Google Analytics script */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-S97V9EQCNQ"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-S97V9EQCNQ');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
