1e7442f40045d73c6c398e484b24d325c4ada6ef
projectinaja <projectinn.id@gmail.com>
2025-11-05 10:00:22 +0700

    update event container bug

--- src/components/Home/EventList/index.tsx
+++ src/components/Home/EventList/index.tsx
@@ -1,12 +1,12 @@
-import Link from 'next/link';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleArrowRight } from '@fortawesome/free-solid-svg-icons';
-import EventCardLoading from '@/components/Card/EventCard/loading';
-import { EventProps } from '@/utils/globalInterface';
-import EventCard from '@/components/Card/EventCard';
-import styles from '../index.module.css';
-import useWindowSize from '@/utils/useWindowSize';
-import React from 'react';
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
+import EventCardLoading from "@/components/Card/EventCard/loading";
+import { EventProps } from "@/utils/globalInterface";
+import EventCard from "@/components/Card/EventCard";
+import styles from "../index.module.css";
+import useWindowSize from "@/utils/useWindowSize";
+import React from "react";
 
 interface ListProps {
   data: EventProps[];
@@ -18,18 +18,16 @@ const EventList = ({ data, loading }: ListProps) => {
 
   return data.length > 0 ? (
     <>
-      <div className='my-12 md:mx-auto md:max-w-7xl md:px-10'>
-        <div className='flex justify-between items-center text-dark mb-4 px-6'>
+      <div className="my-12 md:mx-auto md:max-w-7xl md:px-10">
+        <div className="flex justify-between items-center text-dark mb-4 px-6">
           <h3 className={styles.heading}>Event</h3>
-          <Link href='/event' className='text-primary-base flex gap-2 items-center'>
+          <Link href="/event" className="text-primary-base flex gap-2 items-center">
             Lihat Semua
             <FontAwesomeIcon icon={faCircleArrowRight} />
           </Link>
         </div>
         {!loading ? (
-          <div
-            className={`${styles.eventContainer} min-h-80 gap-1 items-center w-full pb-3 md:px-3 px-0 md:ml-0`}
-          >
+          <div className={`${styles.eventContainer2} min-h-80 gap-1 items-center w-full pb-3 md:px-3 px-0 md:ml-0`}>
             {data.map((event: any) => (
               <div className={styles.eventCard} key={event.id}>
                 <EventCard
@@ -50,9 +48,7 @@ const EventList = ({ data, loading }: ListProps) => {
             ))}
           </div>
         ) : (
-          <div
-            className={`${styles.eventContainer} min-h-80 gap-6 items-center w-full pb-3 md:px-3 px-0 md:ml-0`}
-          >
+          <div className={`${styles.eventContainer} min-h-80 gap-6 items-center w-full pb-3 md:px-3 px-0 md:ml-0`}>
             <EventCardLoading />
             <EventCardLoading />
             <EventCardLoading />
@@ -62,7 +58,7 @@ const EventList = ({ data, loading }: ListProps) => {
     </>
   ) : (
     <>
-      <p className='font-semibold text-dark'>Belum ada event</p>
+      <p className="font-semibold text-dark">Belum ada event</p>
     </>
   );
 };
