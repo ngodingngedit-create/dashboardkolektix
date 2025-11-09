c103fc2ae713a64d3e6b19bc51ce831e2b4cbca0
projectinaja <projectinn.id@gmail.com>
2025-11-09 23:25:46 +0700

    nambahin di config

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ import nextI18NextConfig from "./next-i18next.config.mjs";
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["bedev.kolektix.com", "dev.kolektix.store", "api.kolektix.com", "localhost", "api.kolektix.cloud"],
+    domains: ["bedev.kolektix.com", "dev.kolektix.store", "api.kolektix.com", "localhost", "api.kolektix.cloud", "kolektix.com"],
     // domains: ['dev.kolektix.store'],
   },
   // ...nextI18NextConfig,
